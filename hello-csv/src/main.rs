008d6b9d7f6cd582bc2f9ae430d6082b3844bd96
mihango <mihangokennedy@gmail.com>
2021-12-04 04:26:03 +0300

    avoiding race condition in rust sample

--- hello-csv/src/main.rs
+++ hello-csv/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // \ below escapes the trailing line character
     let penguin_data = "\
     common name, length(cm)
     Little penguin, 33
@@ -9,7 +10,7 @@ fn main() {
     let records = penguin_data.lines();
 
     for (i, record) in records.enumerate() {
-        if i == 0 || record.trim().len() == 0 {
+        if i == 0 || record.trim().len() == 0 { // skip header row and lines with only whitespaces
             continue;
         }
 
@@ -18,7 +19,7 @@ fn main() {
         .map(|field| field.trim())
         .collect();
 
-        if cfg!(debug_assertions) {
+        if cfg!(debug_assertions) { // check configuration at compile time
             eprintln!("debug: {:?} -> {:?}", record, fields);
         }
 
