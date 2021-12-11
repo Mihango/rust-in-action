7be4bf40b38ab73bc1b685a387946d8d8ed597cf
mihango <mihangokennedy@gmail.com>
2021-12-11 16:00:12 +0300

    update message as a struct

--- rust-lifetime/src/main.rs
+++ rust-lifetime/src/main.rs
@@ -6,17 +6,17 @@ fn main() {
 
     for sat_id in sat_ids {
         let mut sat = base.connect(sat_id);
-        base.send(&mut sat, Message::from("Hello Sat"));
 
         println!("t0: {:?}", sat);
+        let mut mailbox = Mailbox::new();
 
-        base.send(&mut sat, Message::from("Hello there!"));
+        base.send(&mut mailbox, Message::new(sat_id, String::from("Hello Sat")));
         println!("t1: {:?}", sat);
-    
-        let msg = sat.recv();
-    
+
+        let msg = sat.recv(&mut mailbox);
+
         println!("t2: {:?}", sat);
-    
+
         println!("msg: {:?}", msg);
     }
 }
@@ -29,7 +29,6 @@ fn find_sat_ids() -> Vec<u64> {
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
-    mailbox: Mailbox,
 }
 
 #[derive(Debug)]
@@ -37,7 +36,48 @@ struct Mailbox {
     messages: Vec<Message>,
 }
 
-type Message = String;
+impl Mailbox {
+
+    fn new() -> Mailbox {
+        Mailbox {
+            messages: vec![]
+        }
+    }
+
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(
+        &mut self,
+        recipient: &CubeSat
+    ) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                let msg = self.messages.remove(i);
+                return Some(msg);
+            }
+        }
+        None
+    }
+}
+
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
+}
+
+impl Message {
+    fn new(to: u64, msg: String) -> Message  {
+        Message {
+            to: to,
+            content: msg,
+        }
+    }
+}
+
+// type Message = String;
 
 struct GroundStation;
 
@@ -45,25 +85,21 @@ impl CubeSat {
     fn new(id: u64) -> CubeSat {
         CubeSat {
             id: id,
-            mailbox: Mailbox {
-                messages: vec![],
-            },
         }
     }
 
     // using references to solve ownership issue
-    fn recv(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(&self)
     }
 }
 
 impl GroundStation {
-    fn send(&self, to: &mut CubeSat, msg: Message) {
-        to.mailbox.messages.push(msg);
+    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
+        mailbox.post(msg);
     }
 
     fn connect(&self, sat_id: u64) -> CubeSat {
         CubeSat::new(sat_id)
     }
 }
-
