a927ee1bf6b4b351a0e22ac4e0637881c2bc7083
mihango <mihangokennedy@gmail.com>
2021-12-13 22:02:27 +0300

    added cpu static function

--- rust-cpu-demo/src/main.rs
+++ rust-cpu-demo/src/main.rs
@@ -3,6 +3,11 @@
 fn main() {
     let mut cpu = CPU::new();
 
+    // 0x8014 indicates:
+    // 8 signifies that the operation involves to registers
+    // 0 maps to cpu.register[0]
+    // 1 maps to cpu.register[1]
+    // s indicates addition
     cpu.current_operation = 0x8014; // opcode that the cpu is to interpret
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
