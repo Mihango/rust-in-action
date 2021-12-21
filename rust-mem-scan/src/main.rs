09bc6585a3bd833eb492f516eab69c7bab7f8685
mihango <mihangokennedy@gmail.com>
2021-12-21 15:44:56 +0300

    added access to memory using box

--- rust-mem-scan/src/main.rs
+++ rust-mem-scan/src/main.rs
@@ -1,4 +1,12 @@
+#![allow(unused)]
+
+use std::f32::consts::PI;
+
 fn main() {
+    memory_scan();
+}
+
+fn memory_scan_with_fault() {
     let mut n_non_zero = 0;
 
     for i in 1..10000 {
@@ -10,4 +18,34 @@ fn main() {
         }
     }
     println!("non-zero bytes in memory: {}", n_non_zero);
+}
+
+static GLOBAL: i32 = 1000;
+
+fn noop() -> *const i32 {
+    let noop_local = 12345;
+    &noop_local as *const i32
+}
+
+fn memory_scan() {
+    let local_str = "a";
+    let local_int = 123;
+    let boxed_str = Box::new('b');
+    let boxed_int = Box::new(789);
+    let fn_int = noop();
+
+    println!("GLOBAL:    {:p}", &GLOBAL as *const i32);
+    println!("local_str: {:p}", local_str as *const str);
+    println!("local_int: {:p}", &local_int as *const i32);
+    println!("boxed_int: {:p}", Box::into_raw(boxed_int));
+    println!("boxed_str: {:p}", Box::into_raw(boxed_str));
+    println!("fn_int:    {:p}", fn_int);
+
+    let pointer = &GLOBAL as *const i32;
+    let result: usize = unsafe {
+        std::mem::transmute(pointer)
+    };
+    unsafe {
+        println!("Result from pointer {:p} is {}", pointer, *pointer);
+    }
 }
