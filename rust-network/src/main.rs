27cd448e9b03611ab5b6286c032f431096b78ff8
mihango <mihangokennedy@gmail.com>
2022-01-22 15:37:28 +0300

    added rand crate

--- rust-network/src/main.rs
+++ rust-network/src/main.rs
@@ -4,7 +4,7 @@ use reqwest;
  
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let url = "http://www.rustinaction.op/";
+    let url = "http://www.rustinaction.com/";
     let response: reqwest::Response = reqwest::get(url).await?;
 
     let content = response.text().await?;
